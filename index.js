0dc8b924d14af45651d1b0d9349b0ed5d9aa11a6
Justin Dennison <justin1dennison@gmail.com>
2020-08-17 12:24:54 -0400

    reorganizing

--- index.js
+++ index.js
@@ -3,7 +3,8 @@
   const filename = "data/ECMWF_ERA-40_subset.nc"
   //const filename = 'data/example_1.nc'
   const nc = await NetCDF.fromFile(filename)
-  for(let variable of Object.values(nc.variables)) {
-     console.log('%s', variable) 
-  }
+  console.log({ nc })
+  //for(let variable of Object.values(nc.variables)) {
+  //   console.log('%s', variable) 
+  //}
 })().catch((e) => console.error(e))
--- src/netcdf.js
+++ src/netcdf.js
@@ -1,4 +1,5 @@
 import { promises as fs } from 'fs'
+import util from 'util'
 import { ByteReader } from '@justin1dennison/bytesjs'
 import { mul, mod, frombuffer, jsonify } from './helpers'
 import {
@@ -12,82 +13,137 @@ import {
   TYPEMAP,
 } from './constants'
 
-export class NetCDF {
-  constructor(source) {
-    this.dimensions = {}
-    this.variables = {}
-    this.attrs = {}
-    this._dims = []
-    this._recs = 0
-    this._recsize = 0
-    this.bytes = ByteReader.of(source)
-    this.magic = this.bytes.string({ length: 3 })
-    this.version = this.bytes.int8()
-    this.format = FORMATCODES[this.version]
-    this.useMMap = false //this will be changed later
-    this.readNumRecs()
-    this.readDimArray()
-    this.readGAttArray()
-    this.readVarArray()
-  }
-  readVarArray() {
-    const header = this.bytes.string({ length: 4 })
-    if (header !== ZERO && header !== NC_VARIABLE)
-      throw new Error('Malformed variable')
-    const count = this.bytes.int32()
+const string = (reader) => {
+  const length = reader.uint32()
+  const name = reader.string({ length })
+  reader.read(mod(-length, 4))
+  return name
+}
+
+const numrecs = (reader) => {
+  const streaming = reader.string({ length: 4 }) === STREAMING
+  reader.rewind(4)
+  const numRecs = streaming ? STREAMING : reader.int32()
+  return { streaming, numRecs }
+}
+
+const dimensions = (reader) => {
+  const absentDimList = reader.string({ length: 8 }) === ABSENT
+  const dimensions = {}
+  const _dims = []
+  if (!absentDimList) {
+    reader.rewind(8)
+    const ncDimension = reader.string({ length: 4 })
+    if (ncDimension !== NC_DIMENSION) throw new Error('Invalid NC Dimension')
+    const count = reader.int32()
     for (let i = 0; i < count; i++) {
-      const {
-        name,
-        dimensions,
-        shape,
-        typecode,
-        attributes,
-        size,
-        dtype,
-        begin,
-        vsize,
-      } = this._readVar()
-      const actualSize = [...shape, size].reduce(mul)
-      const position = this.bytes.position
-      this.bytes.seek(begin)
-      const data = frombuffer(this.bytes.read(actualSize), { dtype, shape })
-      this.bytes.seek(position)
-      this.variables[name] = NetCDFVariable.from({
-        data,
-        name,
-        dimensions,
-        shape,
-        attributes,
-        size,
-        typecode,
-        dtype,
-        begin,
-        vsize,
-      })
+      const name = string(reader)
+      const length = reader.int32()
+      dimensions[name] = length
+      _dims.push(name)
     }
   }
-  _readVar() {
-    const name = this._readString()
-    const dimensions = []
-    const shape = []
-    const dims = this.bytes.int32()
-    for (let i = 0; i < dims; i++) {
-      const dimid = this.bytes.int32()
-      const dimname = this._dims[dimid]
-      dimensions.push(dimname)
-      const dim = this.dimensions[dimname]
-      shape.push(dim)
+  return { absentDimList, dimensions, _dims }
+}
+
+const values = (reader) => {
+  const type = reader.string({ length: 4 })
+  const n = reader.int32()
+  const [typecode, size] = TYPEMAP[type]
+  const count = n * size
+  let values = reader.read(parseInt(count))
+
+  if (typecode !== 'c') {
+    values = frombuffer(values, { dtype: typecode })
+    if (values.shape.length === 1 && values.shape[0] === 1) {
+      values = values[0]
     }
-    const attributes = this.readAttArray()
-    const ncType = this.bytes.read(4)
-    const vsize = this.bytes.int32()
-    const begin = this.version === 1 ? this.bytes.int32() : this.bytes.int64()
-    const [typecode, size] = TYPEMAP[ncType]
-    const dtype = `>${typecode}`
-    return {
+  } else {
+    values = values.toString().trimRight('\x00')
+  }
+  reader.read(mod(-count, 4))
+  return values
+}
+
+const attrs = (reader) => {
+  const attrs = {}
+  const header = reader.string({ length: 4 })
+  if (header !== ZERO && header !== NC_ATTRIBUTE)
+    throw new Error('Unexpected Attribute Header')
+  const count = reader.int32()
+  for (let i = 0; i < count; i++) {
+    const name = string(reader)
+    attrs[name] = values(reader)
+  }
+  return attrs
+}
+const gattrs = (reader) => {
+  return Object.entries(attrs(reader)).reduce((acc, n) => {
+    const [k, v] = n
+    acc[k] = v
+    return acc
+  }, {})
+}
+
+const variable = (reader, { _dims, dimensions, version }) => {
+  const name = string(reader)
+  const ds = []
+  const shape = []
+  const dims = reader.int32()
+  for (let i = 0; i < dims; i++) {
+    const dimid = reader.int32()
+    const dimname = _dims[dimid]
+    ds.push(dimname)
+    const dim = dimensions[dimname]
+    shape.push(dim)
+  }
+  const attributes = attrs(reader)
+  const ncType = reader.read(4)
+  const vsize = reader.int32()
+  const begin = version === 1 ? reader.int32() : reader.int64()
+  const [typecode, size] = TYPEMAP[ncType]
+  const dtype = `>${typecode}`
+  return {
+    name,
+    dimensions,
+    shape: shape[0] === 0 ? shape.slice(1) : shape,
+    attributes,
+    size,
+    typecode,
+    dtype,
+    begin,
+    vsize,
+  }
+}
+
+const variables = (reader, dataset) => {
+  const header = reader.string({ length: 4 })
+  const variables = {}
+  if (header !== ZERO && header !== NC_VARIABLE)
+    throw new Error('Malformed variable')
+  const count = reader.int32()
+  for (let i = 0; i < count; i++) {
+    const {
       name,
       dimensions,
-      shape: shape[0] === 0 ? shape.slice(1) : shape,
+      shape,
+      typecode,
+      attributes,
+      size,
+      dtype,
+      begin,
+      vsize,
+    } = variable(reader, dataset)
+    const actualSize = [...shape, size].reduce(mul)
+    const position = reader.position
+    reader.seek(begin)
+    const data = frombuffer(reader.read(actualSize), { dtype, shape })
+    reader.seek(position)
+    variables[name] = {
+      data,
+      name,
+      dimensions,
+      shape,
       attributes,
       size,
       typecode,
@@ -96,70 +152,25 @@ export class NetCDF {
       vsize,
     }
   }
-  readGAttArray() {
-    for (let [k, v] of Object.entries(this.readAttArray())) {
-      this.attrs[k] = v
-    }
-  }
-  readAttArray() {
-    const header = this.bytes.string({ length: 4 })
-    if (header !== ZERO && header !== NC_ATTRIBUTE)
-      throw new Error('Unexpected Attribute Header')
-    const count = this.bytes.int32()
-    const attrs = {}
-    for (let i = 0; i < count; i++) {
-      const name = this._readString()
-      attrs[name] = this._readValues()
-    }
-    return attrs
-  }
-  _readString() {
-    const count = this.bytes.uint32()
-    const name = this.bytes.string({ length: count })
-    this.bytes.read(mod(-count, 4))
-    return name
-  }
-  _readValues() {
-    const type = this.bytes.string({ length: 4 })
-    const n = this.bytes.int32()
-    const [typecode, size] = TYPEMAP[type]
-    const count = n * size
-    let values = this.bytes.read(parseInt(count))
+  return variables
+}
 
-    if (typecode !== 'c') {
-      values = frombuffer(values, { dtype: typecode })
-      if (values.shape.length === 1 && values.shape[0] === 1) {
-        values = values[0]
-      }
-    } else {
-      values = values.toString().trimRight('\x00')
-    }
-    this.bytes.read(mod(-count, 4))
-    return values
+export class NetCDF {
+  constructor(source) {
+    this.reader = ByteReader.of(source)
+    //TODO: Add validation
+    const magic = this.reader.string({ length: 3 })
+    const version = this.reader.int8()
+    const format = FORMATCODES[version]
+    Object.assign(
+      this,
+      { magic, version, format },
+      numrecs(this.reader),
+      dimensions(this.reader),
+      { attrs: gattrs(this.reader) },
+      // { variables: variables(this.reader, this) }
+    )
   }
-
-  readNumRecs() {
-    this.streaming = this.bytes.string({ length: 4 }) === STREAMING
-    this.bytes.rewind(4)
-    this.numRecs = this.streaming ? STREAMING : this.bytes.int32()
-  }
-  readDimArray() {
-    this.absentDimList = this.bytes.string({ length: 8 }) === ABSENT
-    if (!this.absentDimList) {
-      this.bytes.rewind(8)
-      this.ncDimension = this.bytes.string({ length: 4 })
-      if (this.ncDimension !== NC_DIMENSION)
-        throw new Error('Invalid NC Dimension')
-      const count = this.bytes.int32()
-      for (let i = 0; i < count; i++) {
-        const name = this._readString()
-        const length = this.bytes.int32()
-        this.dimensions[name] = length
-        this._dims.push(name)
-      }
-    }
-  }
-
   static async fromFile(filepath) {
     const source = await fs.readFile(filepath)
     return new NetCDF(source)
@@ -176,7 +187,7 @@ class NetCDFVariable {
     this.attributes = attributes
   }
   static from({
-    name, 
+    name,
     data,
     typecode,
     size,
@@ -195,11 +206,11 @@ class NetCDFVariable {
     )
   }
   get shape() {
-    return this.data.shape 
+    return this.data.shape
   }
   get(...idxs) {
-    return this.data.get(...idxs) 
-  } 
+    return this.data.get(...idxs)
+  }
   toString() {
     return `
     NetCDFVariable(${this.name})
@@ -207,9 +218,6 @@ class NetCDFVariable {
 	Size: ${this.size}
 	Dimensions: ${this.dimensions}
 	Attributes: ${jsonify(this.attributes, 10)}
-	` 
+	`
   }
-} 
-
-
-
+}
